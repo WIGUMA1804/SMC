b13ec7dadbf7b20d6c0b8fdb39d4d97343fa9c4a
James Montealegre <James.MotealegreG@perficient.com>
2021-12-07 15:59:18 -0500

    did some changes about neuronal network

--- neuronal.py
+++ neuronal.py
@@ -4,15 +4,13 @@ import getsuperset
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
 from sklearn import metrics
+from bson import json_util
+from flask import Response
 
 def minmax_norm(df_input):
     return (df_input - df_input.min()) / ( df_input.max() - df_input.min())
     
 def getNeuronal(database):
-    
-    # data_SIF_401=pd.read_csv("C:\DOCTORADO\SEMESTRE8\exportacion\DATOS\SIF_401.csv")
-    # x = data_SIF_401[['MESPAEA_rCurrent_401','SIFOC_sif401_LEC']]
-    # y =data_SIF_401['MESPAEA_udiEnergyConsumed_401']
     
     df = getsuperset.get_superset(database)
     x = df[['MESPAEA_rCurrent_401','SIFOC_sif401_LEC']]
@@ -26,18 +24,29 @@ def getNeuronal(database):
     mlr = MLPRegressor(solver='lbfgs',alpha=1e-5,hidden_layer_sizes=(3,3),random_state=1)
     mlr.fit(X_train_norm, y_train_norm)
     
-    #graficar,se deben enviar por el json
     expected_y  = y_test
     predicted_y = mlr.predict(X_test)
     
     prediction = mlr.predict(X_test)
     error = prediction - y_test
     
-    #se debe enviar
-    print(metrics.r2_score(expected_y, predicted_y))
-    print(metrics.mean_squared_log_error(expected_y, predicted_y))
-    print(mlr.score(X_train_norm, y_train_norm))
-    print(mlr.get_params(deep=True))
+    r2_score = metrics.r2_score(expected_y, predicted_y)
+    mean_squared = metrics.mean_squared_log_error(expected_y, predicted_y)
+    mlr_score = mlr.score(X_train_norm, y_train_norm)
+    mlr_params = mlr.get_params(deep=True)
+    
+    data_dict = dict()
+    data_dict['r2_score'] = r2_score
+    data_dict['mean_squared'] = mean_squared
+    data_dict['mlr_score'] = mlr_score
+    data_dict['mlr_params'] = mlr_params
+    
+    data_dict['expected_y'] = expected_y
+    data_dict['predicted_y'] = predicted_y
+
+    response = json_util.dumps(data_dict)
+    return Response(response, mimetype='application/json')
+
 
 
 
